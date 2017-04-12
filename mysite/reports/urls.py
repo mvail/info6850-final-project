af77d5970ed0d6eb741567fe2c5d7bb74595baeb
mvail <margaret@dal.ca>
2017-04-12 22:44:29 +0000

    Adding new pages for the three reports

--- mysite/reports/urls.py
+++ mysite/reports/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     # ex: /patent/5/
     url(r'^(?P<pid>[0-9]+)/$', views.detail, name='detail'),
     url(r'^datatables/$', views.datatables, name='datatables'),
+    url(r'^charts/$', views.charts, name='charts'),
+    url(r'^keywords/$', views.keywords, name='keywords'),
+    url(r'^categories/$', views.categories, name='categories'),
+    url(r'^complex/$', views.complex, name='complex'),
 ]
--- mysite/reports/views.py
+++ mysite/reports/views.py
@@ -17,10 +17,6 @@ from .models import Uspc
 
 def index(request):
 
-    patents = Patent.objects.all()
-    keywords = Keywords.objects.all()
-    categories = Uspc.objects.all()
-
     topWords_SQL = Keywords.objects.raw('SELECT *, SUM(word_count) as word_count2 FROM reports_keywords GROUP BY word ORDER BY word_count2 DESC LIMIT 10')
 
     topWords = []
@@ -51,9 +47,7 @@ def index(request):
 
 
     return render(request, 'reports/index.html', {
-        'patents': patents,
-        'keywords': keywords,
-        'categories': categories,
+
         'topWords': topWords,
 
     })
@@ -77,16 +71,149 @@ def detail(request, pid):
         raise Http404("Patent does not exist")
     return render(request, 'reports/detail.html', {'patent': patent})
 
-def patent_list(request):
-    patent_list = Patent.objects.all()
-    page = request.GET.get('page', 1)
+def charts(request):
 
-    paginator = Paginator(patent_list, 10)
-    try:
-        patents = paginator.page(page)
-    except PageNotAnInteger:
-        patents = paginator.page(1)
-    except EmptyPage:
-        patents = paginator.page(paginator.num_pages)
+    topWords_SQL = Keywords.objects.raw('SELECT *, SUM(word_count) as word_count2 FROM reports_keywords GROUP BY word ORDER BY word_count2 DESC LIMIT 10')
 
-    return render(request, 'reports/patent_list.html', { 'patents': patents })
+    topWords = []
+
+    for topWord_SQL in topWords_SQL:
+        row = {}
+        word = topWord_SQL.word
+        row['word'] = word
+
+        week1_SQL = Keywords.objects.raw('SELECT * FROM reports_keywords WHERE word = %s AND week = 1', [word])
+        for week1 in week1_SQL:
+            row['week1'] = week1.word_count
+
+        week2_SQL = Keywords.objects.raw('SELECT * FROM reports_keywords WHERE word = %s AND week = 2', [word])
+        for week2 in week2_SQL:
+            row['week2'] = week2.word_count
+
+        week3_SQL = Keywords.objects.raw('SELECT * FROM reports_keywords WHERE word = %s AND week = 3', [word])
+        for week3 in week3_SQL:
+            row['week3'] = week3.word_count
+
+        week4_SQL = Keywords.objects.raw('SELECT * FROM reports_keywords WHERE word = %s AND week = 4', [word])
+        for week4 in week4_SQL:
+            row['week4'] = week4.word_count
+
+        topWords.append(row)
+
+
+
+    return render(request, 'reports/charts.html', {
+
+        'topWords': topWords,
+
+    })
+
+def keywords(request):
+
+    topWords_SQL = Keywords.objects.raw('SELECT *, SUM(word_count) as word_count2 FROM reports_keywords GROUP BY word ORDER BY word_count2 DESC LIMIT 10')
+
+    topWords = []
+
+    for topWord_SQL in topWords_SQL:
+        row = {}
+        word = topWord_SQL.word
+        row['word'] = word
+
+        week1_SQL = Keywords.objects.raw('SELECT * FROM reports_keywords WHERE word = %s AND week = 1', [word])
+        for week1 in week1_SQL:
+            row['week1'] = week1.word_count
+
+        week2_SQL = Keywords.objects.raw('SELECT * FROM reports_keywords WHERE word = %s AND week = 2', [word])
+        for week2 in week2_SQL:
+            row['week2'] = week2.word_count
+
+        week3_SQL = Keywords.objects.raw('SELECT * FROM reports_keywords WHERE word = %s AND week = 3', [word])
+        for week3 in week3_SQL:
+            row['week3'] = week3.word_count
+
+        week4_SQL = Keywords.objects.raw('SELECT * FROM reports_keywords WHERE word = %s AND week = 4', [word])
+        for week4 in week4_SQL:
+            row['week4'] = week4.word_count
+
+        topWords.append(row)
+
+
+
+    return render(request, 'reports/keywords.html', {
+
+        'topWords': topWords,
+
+    })
+
+def categories(request):
+
+    topWords_SQL = Keywords.objects.raw('SELECT *, SUM(word_count) as word_count2 FROM reports_keywords GROUP BY word ORDER BY word_count2 DESC LIMIT 10')
+
+    topWords = []
+
+    for topWord_SQL in topWords_SQL:
+        row = {}
+        word = topWord_SQL.word
+        row['word'] = word
+
+        week1_SQL = Keywords.objects.raw('SELECT * FROM reports_keywords WHERE word = %s AND week = 1', [word])
+        for week1 in week1_SQL:
+            row['week1'] = week1.word_count
+
+        week2_SQL = Keywords.objects.raw('SELECT * FROM reports_keywords WHERE word = %s AND week = 2', [word])
+        for week2 in week2_SQL:
+            row['week2'] = week2.word_count
+
+        week3_SQL = Keywords.objects.raw('SELECT * FROM reports_keywords WHERE word = %s AND week = 3', [word])
+        for week3 in week3_SQL:
+            row['week3'] = week3.word_count
+
+        week4_SQL = Keywords.objects.raw('SELECT * FROM reports_keywords WHERE word = %s AND week = 4', [word])
+        for week4 in week4_SQL:
+            row['week4'] = week4.word_count
+
+        topWords.append(row)
+
+
+
+    return render(request, 'reports/categories.html', {
+
+        'topWords': topWords,
+
+    })
+
+def complex(request):
+
+    topWords_SQL = Keywords.objects.raw('SELECT *, SUM(word_count) as word_count2 FROM reports_keywords GROUP BY word ORDER BY word_count2 DESC LIMIT 10')
+
+    topWords = []
+
+    for topWord_SQL in topWords_SQL:
+        row = {}
+        word = topWord_SQL.word
+        row['word'] = word
+
+        week1_SQL = Keywords.objects.raw('SELECT * FROM reports_keywords WHERE word = %s AND week = 1', [word])
+        for week1 in week1_SQL:
+            row['week1'] = week1.word_count
+
+        week2_SQL = Keywords.objects.raw('SELECT * FROM reports_keywords WHERE word = %s AND week = 2', [word])
+        for week2 in week2_SQL:
+            row['week2'] = week2.word_count
+
+        week3_SQL = Keywords.objects.raw('SELECT * FROM reports_keywords WHERE word = %s AND week = 3', [word])
+        for week3 in week3_SQL:
+            row['week3'] = week3.word_count
+
+        week4_SQL = Keywords.objects.raw('SELECT * FROM reports_keywords WHERE word = %s AND week = 4', [word])
+        for week4 in week4_SQL:
+            row['week4'] = week4.word_count
+
+        topWords.append(row)
+
+
+    return render(request, 'reports/complex.html', {
+
+        'topWords': topWords,
+
+    })
