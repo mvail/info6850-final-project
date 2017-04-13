de9a1df60b5b7d818b06524835e3237d6623e428
mvail <margaret@dal.ca>
2017-04-13 13:22:13 +0000

    Update uspc reports

--- mysite/reports/views.py
+++ mysite/reports/views.py
@@ -110,6 +110,8 @@ def charts(request):
 
 def keywords(request):
 
+    keywords = Keywords.objects.all()
+
     topWords_SQL = Keywords.objects.raw('SELECT *, SUM(word_count) as word_count2 FROM reports_keywords GROUP BY word ORDER BY word_count2 DESC LIMIT 10')
 
     topWords = []
@@ -140,46 +142,109 @@ def keywords(request):
 
 
     return render(request, 'reports/keywords.html', {
-
+        'keywords': keywords,
         'topWords': topWords,
 
     })
 
 def categories(request):
 
-    topWords_SQL = Keywords.objects.raw('SELECT *, SUM(word_count) as word_count2 FROM reports_keywords GROUP BY word ORDER BY word_count2 DESC LIMIT 10')
+    top10_categories_week1 = Uspc.objects.filter(week=1).order_by('-patent_count')[:10]
+    top10_categories_week2 = Uspc.objects.filter(week=2).order_by('-patent_count')[:10]
+    top10_categories_week3 = Uspc.objects.filter(week=3).order_by('-patent_count')[:10]
+    top10_categories_week4 = Uspc.objects.filter(week=4).order_by('-patent_count')[:10]
 
-    topWords = []
+    topCategories_SQL = Uspc.objects.raw('SELECT *, SUM(patent_count) as patent_count2 FROM reports_uspc GROUP BY uspc ORDER BY patent_count2 DESC LIMIT 10')
 
-    for topWord_SQL in topWords_SQL:
+    topCategories = []
+
+    for topCategory_SQL in topCategories_SQL:
         row = {}
-        word = topWord_SQL.word
-        row['word'] = word
+        uspc = topCategory_SQL.uspc
+        row['uspc'] = uspc
 
-        week1_SQL = Keywords.objects.raw('SELECT * FROM reports_keywords WHERE word = %s AND week = 1', [word])
+        week1_SQL = Uspc.objects.raw('SELECT * FROM reports_uspc WHERE uspc = %s AND week = 1', [uspc])
         for week1 in week1_SQL:
-            row['week1'] = week1.word_count
+            row['week1'] = week1.patent_count
+        if 'week1' not in row:
+            row['week1'] = 0
 
-        week2_SQL = Keywords.objects.raw('SELECT * FROM reports_keywords WHERE word = %s AND week = 2', [word])
+        week2_SQL = Uspc.objects.raw('SELECT * FROM reports_uspc WHERE uspc = %s AND week = 2', [uspc])
         for week2 in week2_SQL:
-            row['week2'] = week2.word_count
+            row['week2'] = week2.patent_count
+        if 'week2' not in row:
+            row['week2'] = 0
 
-        week3_SQL = Keywords.objects.raw('SELECT * FROM reports_keywords WHERE word = %s AND week = 3', [word])
+        week3_SQL = Uspc.objects.raw('SELECT * FROM reports_uspc WHERE uspc = %s AND week = 3', [uspc])
         for week3 in week3_SQL:
-            row['week3'] = week3.word_count
+            row['week3'] = week3.patent_count
+        if 'week3' not in row:
+            row['week3'] = 0
 
-        week4_SQL = Keywords.objects.raw('SELECT * FROM reports_keywords WHERE word = %s AND week = 4', [word])
+        week4_SQL = Uspc.objects.raw('SELECT * FROM reports_uspc WHERE uspc = %s AND week = 4', [uspc])
         for week4 in week4_SQL:
-            row['week4'] = week4.word_count
+            row['week4'] = week4.patent_count
+        if 'week4' not in row:
+            row['week4'] = 0
 
-        topWords.append(row)
+        topCategories.append(row)
 
+    categoriesChangedByTenPercent = []
 
+    categories_SQL = Uspc.objects.raw('SELECT DISTINCT uspc, id FROM reports_uspc ORDER BY uspc ASC')
+
+    for category_SQL in categories_SQL:
+        row = {}
+        uspc = category_SQL.uspc
+        row['uspc'] = uspc
+
+        week1_SQL = Uspc.objects.raw('SELECT * FROM reports_uspc WHERE uspc = %s AND week = 1', [uspc])
+        for week1 in week1_SQL:
+            row['week1'] = week1.patent_count
+        if 'week1' not in row:
+            row['week1'] = 0
+
+        week2_SQL = Uspc.objects.raw('SELECT * FROM reports_uspc WHERE uspc = %s AND week = 2', [uspc])
+        for week2 in week2_SQL:
+            row['week2'] = week2.patent_count
+        if 'week2' not in row:
+            row['week2'] = 0
+
+        week3_SQL = Uspc.objects.raw('SELECT * FROM reports_uspc WHERE uspc = %s AND week = 3', [uspc])
+        for week3 in week3_SQL:
+            row['week3'] = week3.patent_count
+        if 'week3' not in row:
+            row['week3'] = 0
+
+        week4_SQL = Uspc.objects.raw('SELECT * FROM reports_uspc WHERE uspc = %s AND week = 4', [uspc])
+        for week4 in week4_SQL:
+            row['week4'] = week4.patent_count
+        if 'week4' not in row:
+            row['week4'] = 0
+
+        values = []
+        values.append(row['week1']);
+        values.append(row['week2']);
+        values.append(row['week3']);
+        values.append(row['week4']);
+
+        for a, b in zip(values[::1], values[1::1]):
+            if a == 0:
+                continue
+            if 100 * (b - a) / a > 9:
+                if row in categoriesChangedByTenPercent:
+                    continue
+                else:
+                    categoriesChangedByTenPercent.append(row)
+                continue
 
     return render(request, 'reports/categories.html', {
-
-        'topWords': topWords,
-
+        'top10_categories_week1': top10_categories_week1,
+        'top10_categories_week2': top10_categories_week2,
+        'top10_categories_week3': top10_categories_week3,
+        'top10_categories_week4': top10_categories_week4,
+        'topCategories': topCategories,
+        'categoriesChangedByTenPercent': categoriesChangedByTenPercent,
     })
 
 def complex(request):
